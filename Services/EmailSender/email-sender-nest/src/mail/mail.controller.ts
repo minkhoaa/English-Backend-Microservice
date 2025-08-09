ae9af41362f6cdaf2aefc0d1a59c3f674009ad9b
minkhoaa <tukhoa040505@gmail.com>
2025-08-09 19:45:10 +0700

    config swagger for emailSender

--- Services/EmailSender/email-sender-nest/src/mail/mail.controller.ts
+++ Services/EmailSender/email-sender-nest/src/mail/mail.controller.ts
@@ -2,20 +2,33 @@ import { Body, Controller, Get, Post } from "@nestjs/common";
 import { IsEmail, IsNotEmpty, IsObject, IsOptional, IsString } from "class-validator";
 import { MailService } from "./mail.service";
 import { ok } from "assert";
-
+import { ApiTags, ApiResponse, ApiProperty, ApiPropertyOptional  } from "@nestjs/swagger";
+import { time } from "console";
+import { title } from "process";
+import { url } from "inspector";
 class SendEmailDto {
+  @ApiProperty({example:'user@example.com'})
   @IsEmail() to!: string;
+  @ApiProperty({example: 'string'})
   @IsString() @IsNotEmpty() subject!: string;
+  @ApiProperty({example:'string'})
   @IsString() @IsNotEmpty() template!: string; // vd: 'new-content'
+
+  @ApiPropertyOptional({
+    type:'object',
+    additionalProperties: true,
+    example: {title: 'Unit 12', url: 'https://app.local/lesson/12'}
+  })
   @IsObject() @IsOptional() variables?: Record<string, any>;
 }
 
-
-@Controller()
+@ApiTags('Email')
+@Controller('emailcontroller')
 export class MailController {
     constructor(private readonly mail: MailService) {}
 
-    @Post('emails')
+    @Post('send')
+    @ApiResponse({status: 200, description:'Email sent'})
     public async send(@Body() dto: SendEmailDto) {
         await this.mail.send(dto);
         return {ok:true};
